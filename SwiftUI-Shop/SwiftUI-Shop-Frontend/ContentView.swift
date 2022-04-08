a3c27f021ae8feb5a873d32ab3bead2687075984
Ewald <57798772+EforestHD@users.noreply.github.com>
2022-04-08 12:45:51 +0200

    init App

---
+++ SwiftUI-Shop/SwiftUI-Shop-Frontend/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-Shop-Frontend
+//
+//  Created by Ewald Kerner on 08.04.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hi! Please feel free to contact me via Twitter @EwaldKerner .")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI-Shop/SwiftUI-Shop-Frontend/SwiftUI_Shop_FrontendApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Shop_FrontendApp.swift
+//  SwiftUI-Shop-Frontend
+//
+//  Created by Ewald Kerner on 08.04.22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Shop_FrontendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
